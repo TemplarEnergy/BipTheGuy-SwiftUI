4217e07ff348bfef369a2c1f1a82e514b9084751
Tom Radford <tom@radfords-web.com>
2023-01-25 18:47:50 -0400

    Initial Commit

---
+++ BipTheGuy-SwiftUI/BipTheGuy_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuy_SwiftUIApp.swift
+//  BipTheGuy-SwiftUI
+//
+//  Created by Thomas Radford on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuy_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
